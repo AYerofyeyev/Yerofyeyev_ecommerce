62594415510f46321828c10f28fa67390819edaa
Aleksandr Yerofyeyev <a.yerofyeyev@gmail.com>
2018-09-05 17:49:57 +0300

    content blocks

--- src_fe/js/index.js
+++ src_fe/js/index.js
@@ -2,6 +2,11 @@ import '../css/main.scss';
 import '../img/logo.png';
 import '../img/search.svg';
 import '../img/cart-white.svg';
+import '../img/long-arrow-alt-right-solid.svg';
+import '../img/120393-1.jpg';
+import '../img/120393-2.jpg';
+import '../img/120393-3.jpg';
+import '../img/120393-4.jpg';
 
 console.log('script is connected');
 (function () {
