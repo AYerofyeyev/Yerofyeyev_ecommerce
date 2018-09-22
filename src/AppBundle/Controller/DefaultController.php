5586b7445e2ffbd65905713e28c6170480c1c070
Aleksandr Yerofyeyev <a.yerofyeyev@gmail.com>
2018-09-22 20:08:53 +0300

    DataBase and feedback form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Feedback;
+use AppBundle\Form\FeedbackType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,35 +13,63 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Template()
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         // replace this example code with whatever you need
 //        return $this->render('default/index.html.twig', [
 //            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
 //        ]);
-        return $this->render('@App/Default/index.html.twig');
+//        return $this->render('@App/Default/index.html.twig');
     }
 
     /**
      * @Route("/mobile", name="mobile")
+     * @Template()
      */
-    public function mobileAction(Request $request)
+    public function mobileAction()
     {
         $products = [
-            ['id' => 120393],
-            ['id' => 120394]
+            1 => 120393,
+            120394
         ];
+        return ['products' => $products];
+    }
+
+    /**
+     * @Route("/{id}", name="mobile_item", requirements={"id": "[0-9]+"})
+     */
+    public function showAction($id)
+    {
+        $products = [
+            1 => $id
+        ];
+
         return $this->render('@App/Default/mobile.html.twig', ['products' => $products]);
     }
 
     /**
-     * @Route("/mobile/{id}", name="mobile_item", requirements={})
+     * @Route("/feedback", name="feedback")
+     * @Template()
      */
-    public function showAction($id)
+    public function feedbackAction(Request $request)
     {
-        $product = [
+        $feedback = new Feedback();
+        $feedback->setIpAddress($request->getClientIp());
+        $form = $this->createForm(FeedbackType::class, $feedback);
+        $form->handleRequest($request);
 
-        ];
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($feedback);
+            $em->flush();
+
+            $this->addFlash('success', 'Message sent.');
+            return $this->redirectToRoute('feedback');
+        }
+
+        return ['feedback_form' => $form->createView()];
     }
 }
